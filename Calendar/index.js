4891e88471ea4594bb3b10b05cecc31473ceb449
september-choe <september.choe.23@gmail.com>
2020-03-25 22:21:29 +0900

    Set calendar date 1. set first date, last date 2. set calendar col, row algorithm

--- Calendar/index.js
+++ Calendar/index.js
@@ -1,11 +1,17 @@
 var calendarTitle = document.getElementById('calendarTitle');
-var date = new Date();
+var date = new Date(Date.now());
 
 calendarTitle.innerHTML = date.getMonth()+1 + '월';
 
-var firstDate = new Date(date.getFullYear, date.getMonth+1,1);
-var lastDate = new Date(date.getFullYear,);
+var firstDate = new Date(date.getFullYear(), date.getMonth(),1);
+var lastDate = new Date(date.getFullYear(), date.getMonth()+1,0);
 
-for (var i = 0; i < 7; i++){
-asdfasdf;
+var seven = 7;
+
+// calendar has 7 cols
+// firstDate goes into...
+for (var i = 1; i <= seven; i++){
+   // (date.getDate / 7) * 10 + 1 --> calendar row
+   // (date.getDate % 7)  --> calendar col
+
 } 
